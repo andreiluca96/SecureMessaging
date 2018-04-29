ab9106076ec56657d963522ee28e08a4651b478b
DESKTOP-L8H23GJ\Luca Andrei <luca.andrei96@gmail.com>
2018-04-29 19:01:28 +0300

    Started working on conversation list view and created basic layout with mocks.

--- app/src/main/java/com/example/andrluc/securemessaging/MainActivity.java
+++ app/src/main/java/com/example/andrluc/securemessaging/MainActivity.java
@@ -1,13 +1,74 @@
 package com.example.andrluc.securemessaging;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<String> users = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        users.add("user1");
+        users.add("user2");
+        users.add("user3");
+        users.add("user4");
+        users.add("user5");
+        users.add("user6");
+        users.add("user7");
+        users.add("user8");
+        users.add("user9");
+
+        ListView conversationListView = findViewById(R.id.conversationListView);
+        ConversationItemAdapter conversationItemAdapter = new ConversationItemAdapter();
+        conversationListView.setAdapter(conversationItemAdapter);
+
+    }
+
+    class ConversationItemAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return users.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @SuppressLint({"ViewHolder", "InflateParams"})
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            view = getLayoutInflater().inflate(R.layout.conversation_list_view_item, null);
+
+            TextView usernameTextView = view.findViewById(R.id.usernameTextView);
+            TextView lastMessageTextView = view.findViewById(R.id.lastMessageTextView);
+            TextView lastMessageDateTextView = view.findViewById(R.id.lastMessageDateTextView);
+
+            usernameTextView.setText(users.get(i));
+            lastMessageTextView.setText(users.get(i));
+            lastMessageDateTextView.setText("some date");
+
+            return view;
+        }
     }
 }
